2f75dcbdc5199f33983db7e3a1572b7748460c5d
5MDM <110855261+5MDM@users.noreply.github.com>
2023-07-18 21:29:14 +0000

    added quad tree

--- assets/js/game/world-generation.js
+++ assets/js/game/world-generation.js
@@ -2,6 +2,8 @@ import {createNoise2D} from "../lib/perlin.js";
 import {blockData, newBlock} from "./blocks.js";
 import {Mesh, Group, Box3, Vector3, Box3Helper} from "three";
 import {newBox} from "../lib/framework.js";
+import {cam} from "./camera.js";
+import {Quadtree} from "../lib/quadrant.js";
 const bd = await blockData;
 
 // Seed: 0
@@ -14,76 +16,47 @@ function getElevation(x, y) {
 }
 
 const CHUNK_SIZE = 8;
-const BVOLUME_SIZE = 8;
 const grassM = bd.data[bd.name["Grass"]];
 const stoneM = bd.data[bd.name["Stone"]];
+
+cam.camera.position.x = CHUNK_SIZE / 2;
+cam.camera.position.z = CHUNK_SIZE / 2;
 
 var finishGeneration;
 export const blockArray = new Promise(res => {
   finishGeneration = res;
 });
 
-function worldLog(world) {
-  var y = 0;
-  var x;
-  var lx;
-  
-  for(const i in world) {
-    for(const z in world[i]) {
-      const yy = Object.keys(world[i][z]).length;
-      if(y < yy) y = yy;
-    }
-    
-    lx = x;
-    x = Object.keys(world[i]).length;
-    
-    if(lx != undefined)
-      if(lx != x) console.warn("World error");
-  }
-  
-  console.log(
-    `Width: ${x}\n`
-  + `Max height: ${y}\n`
-  + `Length: ${Object.keys(world).length}`
-  );
-}
-
 export function generateWorld(scene) {
   var color = Math.random() * 0xffffff;
-  const world = {};
-  var xcounter = BVOLUME_SIZE;
-  var ycounter = BVOLUME_SIZE;
   
-  var currentGroup = new Group();
+  const world = {};
   const volumeGroups = [];
-  const sizeGroups = [];
   
   for(let i = CHUNK_SIZE; i > 0; i--) {
     // goes sideways / x-axis
-    const yc = i - CHUNK_SIZE / 2;
-    
+    const yc = i;
     for(let z = CHUNK_SIZE; z > 0; z--) {
       // goes down / y-axis
-      const xc = z - CHUNK_SIZE / 2;
-      
+      const xc = z;
       add_blocks({xc, yc});
     }
   }
   
   // call this function for every block made
+  const treeBox = new Box3(
+    //new Vector3(-CHUNK_SIZE / 2, -CHUNK_SIZE / 2, -CHUNK_SIZE / 2),
+    new Vector3(0.5, -7, 0.5),
+    new Vector3(CHUNK_SIZE+0.5, -4, CHUNK_SIZE+0.5),
+  );
+  
+  const tree = new Quadtree(treeBox);
+  
+  scene.add(new Box3Helper(treeBox, 0xffff00));
+  
   function add_block(block) {
+    
     scene.add(block);
-    xcounter--;
-    if(xcounter == 0) {
-      xcounter = BVOLUME_SIZE;
-      ycounter--;
-      if(ycounter == 0) {
-        ycounter = BVOLUME_SIZE;
-        volumeGroups.push(currentGroup);
-        color = Math.random() * 0xffffff;
-        currentGroup = new Group();
-      }
-    }
   }
   
   function add_blocks({xc, yc}) {
@@ -106,5 +79,5 @@ export function generateWorld(scene) {
   }
   
   finishGeneration();
-  return {world, volumeGroups, sizeGroups};
+  return {world, volumeGroups};
 }
---
+++ assets/js/lib/quadrant.js
@@ -0,0 +1,116 @@
+import {Vector3, Box3} from "three";
+
+export class Quadtree {
+  objects = [];
+  subdivided = false;
+  children = [];
+  
+  // boundary: new THREE.Box3()
+  // capacity: positive number
+  constructor(boundary, capacity = 4) {
+    this.boundary = boundary;
+    this.capacity = capacity;
+  }
+  
+  // object: new THREE.Box3()
+  // ^ object.setFromObject(<insert mesh>) is recommended
+  insert(object) {
+    if(!this.boundary.containsPoint(object))
+      return false;
+    
+    // Normal insert
+    if(this.objects.length < this.capacity) {
+      this.objects.push(object);
+      return true;
+    }
+
+    if(!this.subdivided) this.subdivide();
+    
+    for(let i = 0; i < this.children.length; i++)
+      if(this.children[i].insert(object)) return true;
+
+    return false;
+  }
+
+  subdivide() {
+    const box = this.boundary;
+    const center = new Vector3();
+    box.getCenter(center);
+    
+    const min = box.min;
+    const max = box.max;
+
+    const nw = new Box3(min, center);
+    const ne = new Box3(
+      new Vector3(center.x, min.y, min.z),
+      new Vector3(max.x, center.y, center.z),
+    );
+    
+    const sw = new Box3(
+      new Vector3(min.x, center.y, center.z),
+      new Vector3(center.x, max.y, max.z),
+    );
+    
+    const se = new Box3(center, max);
+
+    this.children.push(new Quadtree(nw, this.capacity));
+    this.children.push(new Quadtree(ne, this.capacity));
+    this.children.push(new Quadtree(sw, this.capacity));
+    this.children.push(new Quadtree(se, this.capacity));
+
+    this.subdivided = true;
+  }
+
+  retrieve(object) {
+    const foundObjects = [];
+    
+    // if object is not in boundary
+    if(!this.boundary.intersectsBox(object))
+      return foundObjects;
+    
+    for(let i = 0; i < this.objects.length; i++)
+      if(object !== this.objects[i] 
+      && object.intersectsBox(this.objects[i]))
+        foundObjects.push(this.objects[i]);
+    
+    if(this.subdivided)
+      for(let i = 0; i < this.children.length; i++)
+        foundObjects.push(...this.children[i].retrieve(object));
+    
+    return foundObjects;
+  }
+}
+/*
+// Create objects
+var numObjects = 100;
+var objectSize = 1;
+var objectMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+var quadtreeBoundary = new Box3(
+  new Vector3(-50, -50, -50),
+  new Vector3(50, 50, 50),
+);
+
+var quadtree = new Quadtree(quadtreeBoundary);
+
+// Check for collisions
+function checkCollisions() {
+  scene.children.forEach(function(object) {
+    if(object !== player 
+    && object.geometry instanceof THREE.BoxGeometry) {
+      var objectBB = new THREE.Box3().setFromObject(object);
+
+      var nearbyObjects = quadtree.retrieve(objectBB);
+
+      for (var i = 0; i < nearbyObjects.length; i++) {
+        var nearbyObject = nearbyObjects[i];
+        if (objectBB.intersectsBox(nearbyObject)) {
+          object.material.color.set(0x00ff00); // Collided with another object
+          break;
+        }
+      }
+    }
+  });
+}
+// Render loop
+*/
