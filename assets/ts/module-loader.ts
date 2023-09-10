2f0a4789da21eee00a1715bb843321d6f76a0e45
5MDM <110855261+5MDM@users.noreply.github.com>
2023-09-10 03:33:34 +0000

    added error handling

--- assets/ts/module-loader.ts
+++ assets/ts/module-loader.ts
@@ -1,4 +1,27 @@
-(function() {
+(async function() {
   "use strict";
-  import("./main");
+  try {
+    await import("./main");
+  } catch(err: any) {
+    function $(e: string) {
+      return <HTMLElement>document.querySelector(e);
+    }
+    
+    $("#loading-c")!.style.display = "none";
+    $("#error-overlay")!.style.display = "flex";
+    $("#error-overlay #error-txt")!.innerText = err;
+    $("#error-overlay #error-stack")!.innerText = 
+    err.stack || "Stack is undefined";
+    
+    $("#error-overlay #copy-error")
+    .addEventListener("pointerup", () => {
+      if("clipboard" in navigator) {
+        navigator.clipboard.writeText(`${err}\n${err.stack}`)
+        .then(() => alert("Copied!"))
+        .catch(() => alert("Couldn't copy"))
+      } else {
+        alert("Clipboard is unsupported in your browser");
+      }
+    });
+  }
 })();
